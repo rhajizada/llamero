0ff58a41e98d7949518ac3d5e62282c3f3f13886
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-11-24 00:04:22 -0500

    adjusting toast icons

--- ui/components/ui/sonner.tsx
+++ ui/components/ui/sonner.tsx
@@ -1,28 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import {
-  CircleCheckIcon,
-  InfoIcon,
-  Loader2Icon,
-  OctagonXIcon,
-  TriangleAlertIcon,
-} from "lucide-react"
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, type ToasterProps } from "sonner"
+import * as React from "react";
+import { Loader2Icon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
+        success: null,
+        info: null,
+        warning: null,
+        error: null,
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
       style={
@@ -35,7 +29,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
